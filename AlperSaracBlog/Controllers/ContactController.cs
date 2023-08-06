0c37791a648f12ebaee58a9cfce409179cb137f8
alpersarac <alper.sarac42@gmail.com>
2023-08-06 16:56:47 +0300

    stable and deployed version

--- AlperSaracBlog/Controllers/ContactController.cs
+++ AlperSaracBlog/Controllers/ContactController.cs
@@ -8,5 +8,13 @@ namespace AlperSaracBlog.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SubmitForm(string name, string email, string phone, string message)
+        {
+            // Process the form data here (e.g., send an email, save to database, etc.)
+
+            // Return a success or error response based on your logic
+            return Ok(); // You can customize the response as needed
+        }
     }
 }
